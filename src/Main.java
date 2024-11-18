a28cccaeda428ab43b92825091fb1a060fa9edd9
Kristina047 <ch.drobot@gmail.com>
2024-11-18 12:04:00 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Ты совершеннолетний!");
+        } else {
+            System.out.println("Ты несовершеннолетний, нужно немного подождать.");
+        }
+
+
     }
 }
