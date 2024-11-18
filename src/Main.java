f04b3824b66d676909f7b5baa67a470f48181ed9
Kristina047 <ch.drobot@gmail.com>
2024-11-18 14:15:23 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,42 @@ public class Main {
         System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) {
-            System.out.println("Ты совершеннолетний!");
+            System.out.println("Если возраст человека больше или равен 18, то он совершеннолетний");
         } else {
-            System.out.println("Ты несовершеннолетний, нужно немного подождать.");
+            System.out.println("Если возраст человека меньше 18, то он не достиг совершеннолетия, нужно немного подождать");
         }
 
         System.out.println("Задача 2");
         int temperature = 6;
         if (temperature < 5) {
-            System.out.println("На улице холодно,нужно надеть шапку.");
+            System.out.println("На улице ниже 5 градусов,нужно надеть шапку");
         } else {
-            System.out.println("На улице тепло, можно идти без шапки!");
+            System.out.println("На улице выше 5 градусов, можно идти без шапки!");
         }
         System.out.println("Задача 3");
         int speed = 55;
         if (speed > 60) {
-            System.out.println("Придется платить штраф.");
+            System.out.println("Если скорость больше 60 км/ч, то придется заплатить штраф.");
         } else {
-            System.out.println("Можно ездить спокойно!");
+            System.out.println("Если скорость ниже 60 км/ч, то можно ездить спокойно!");
         }
+
+        System.out.println("Задача 4");
+        int years = 25;
+        if (years >= 2 && years <= 6) {
+            System.out.println(" Если человеку от 2 до 6 лет, то ему нужно ходить в детский сад.")   ;
+        }
+        if (years >= 7 && years <= 17) {
+            System.out.println("  Если человеку от 7 до 17 лет, то ему нужно ходить в школу")   ;
+        }
+        if (years >= 18 && years <= 24) {
+            System.out.println("Если человеку больше 18 лет, но меньше 24, то его место в университете.")   ;
+        }
+        if (years >24) {
+            System.out.println("Если человеку больше 24, то ему пора ходить на работу.")   ;
+        }
+
+        System.out.println("Задача 5");
 
 
 
